2c0217f39c5aa1ef15b45bee9c30c26d41c6d4a8
Krasav4ik-02 <71172716+Krasav4ik-02@users.noreply.github.com>
2023-06-24 21:17:34 +0600

    Создали комментарий

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+from post import views
 from post.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', post_view , name='post_view'),
     path('posts/' , Create_post.as_view() , name = 'posts'),
     path('users/', include('users.urls')),
-    path('post_view/', post_view, name = 'post_view')
-
+    path('post_view/', post_view, name = 'post_view'),
+    path('comment/add/<int:post_id>/', views.add_comment, name='add_comment'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth import get_user
-from post.forms import Posts , Comment
-from post.models import Post
+from post.forms import Posts , Comments
+from post.models import Post, Comment
 
 
 class Create_post(View):
@@ -16,9 +16,26 @@ class Create_post(View):
             new_post = bount_form.save(commit=False)
             new_post.account = get_user(request)
             new_post.save()
-            return render(request, 'home.html')
+            return render(request, 'base.html')
         return render(request, 'posts.html', context={'form': bount_form})
 
 def post_view(request):
     posts = Post.objects.order_by('-date_created_post')
     return render(request, 'post_view.html', {'posts': posts})
+
+
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method == 'POST':
+        form = Comments(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.account = get_user(request)
+            comment.post = post
+            comment.save()
+            return redirect('post_view')
+    else:
+        form = Comments()
+
+    return render(request, 'add_comment.html', {'form': form, 'post_id': post_id})
